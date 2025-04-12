cda4339030f801367e4b043aa20e0a7347c88cec
Nicole <nicolelogan@hotmail.com>
2025-04-12 12:07:27 -0700

    styling single project page buttons, added the functions so that text shows up where it should, etc

--- functions.php
+++ functions.php
@@ -58,11 +58,43 @@ add_action( 'pre_get_posts', 'load_projects_in_archive' );
 function nl_github_button_shortcode() {
     $github = get_post_meta( get_the_ID(), '_nl_github_link', true );
     if ( $github ) {
-        return '<a class="wp-block-button__link wp-element-button" href="' . esc_url( $github ) . '" target="_blank" rel="noopener">Github Repo</a>';
+        return '<a class="wp-block-button__link wp-element-button-github" href="' . esc_url( $github ) . '" target="_blank" rel="noopener">Github Repo</a>';
     }
     return '';
 }
 add_shortcode( 'github_button', 'nl_github_button_shortcode' );
+
+// Live button shortcode
+function nl_live_button_shortcode() {
+    $live = get_post_meta( get_the_ID(), '_nl_live_link', true );
+    if ( $live ) {
+        return '<a class="wp-block-button__link wp-element-button-live" href="' . esc_url( $live ) . '" target="_blank" rel="noopener">Live Project</a>';
+    }
+    return '';
+}
+add_shortcode( 'live_button', 'nl_live_button_shortcode' );
+
+// Shortcode for Project Role
+function nl_project_role_shortcode() {
+    $role = get_post_meta( get_the_ID(), '_nl_project_role', true );
+    if ( $role ) {
+        return '<p><strong>Role:</strong> ' . esc_html( $role ) . '</p>';
+    }
+    return '';
+}
+add_shortcode( 'project_role', 'nl_project_role_shortcode' );
+
+// Shortcode for Project Teammates
+function nl_project_teammates_shortcode() {
+    $teammates = get_post_meta( get_the_ID(), '_nl_project_teammates', true );
+    if ( $teammates ) {
+        return '<p><strong>Teammates:</strong> ' . esc_html( $teammates ) . '</p>';
+    }
+    return '';
+}
+add_shortcode( 'project_teammates', 'nl_project_teammates_shortcode' );
+
+
 
 
 
